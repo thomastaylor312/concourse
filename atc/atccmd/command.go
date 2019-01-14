da8f0443ab7f2e04f2d3d7d93031c1908c11c9ce
Josh Winters <jwinters@pivotal.io>
2019-01-14 16:34:15 -0500

    atc: adds an artifact collector
    Signed-off-by: Krishna Mannem <kmannem@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -742,6 +742,7 @@ func (cmd *RunCommand) constructBackendMembers(
 	dbWorkerLifecycle := db.NewWorkerLifecycle(dbConn)
 	dbResourceCacheLifecycle := db.NewResourceCacheLifecycle(dbConn)
 	dbContainerRepository := db.NewContainerRepository(dbConn)
+	dbArtifactLifecycle := db.NewArtifactLifecycle(dbConn)
 	resourceConfigCheckSessionLifecycle := db.NewResourceConfigCheckSessionLifecycle(dbConn)
 	dbBuildFactory := db.NewBuildFactory(dbConn, lockFactory, cmd.GC.OneOffBuildGracePeriod)
 	bus := dbConn.Bus()
@@ -787,6 +788,7 @@ func (cmd *RunCommand) constructBackendMembers(
 				gc.NewResourceCacheUseCollector(dbResourceCacheLifecycle),
 				gc.NewResourceConfigCollector(dbResourceConfigFactory),
 				gc.NewResourceCacheCollector(dbResourceCacheLifecycle),
+				gc.NewArtifactCollector(dbArtifactLifecycle),
 				gc.NewVolumeCollector(
 					dbVolumeRepository,
 					cmd.GC.MissingGracePeriod,
